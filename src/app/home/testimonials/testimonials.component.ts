b9c3613cafd313ad02db52aed606efe405a9dedb
Ehab <ehab.aa@outlook.com>
2023-07-03 16:57:38 +0200

    testimonials/1

--- src/app/home/testimonials/testimonials.component.ts
+++ src/app/home/testimonials/testimonials.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class TestimonialsComponent {
 
+  title = " Hi, I'am Colin .. "
+  para1 = " a profisional phtographer from England living and working in Vienna, Austria. ";
+
+  para21 = " Outdoor Photo Session" ;
+  para21image = "./../../../assets/girl-1026246_1280.jpg";
+
+  para22 = " Inhouse photo sessions. ";
+  para22image = "./../../../assets/photo-session-in-bodysuit-1759810_1280.jpg";
+
+  para31 = " I love to work with people"
+  para32 = "Use lighting and set poses"
+  para33 = " & "
+  para34 =" Get the best out of them";
+  para4 = " Most of my clients, rather who did not have a sessions before with a proffessional photographer or those who are use to it, go home with photos that they do really love. ";
+  slogan1 = "Great Photos .. Happy Clients ";
+
 }
