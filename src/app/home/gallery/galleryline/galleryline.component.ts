42cbaad7c4675ccbeb4d394f33485feb1e9ec769
AdamAliG <electron.adam@proton.me>
2023-07-04 09:28:43 +0200

    center grid

--- src/app/home/gallery/galleryline/galleryline.component.ts
+++ src/app/home/gallery/galleryline/galleryline.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-galleryline',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./galleryline.component.scss']
 })
 export class GallerylineComponent {
-
+  
 }
