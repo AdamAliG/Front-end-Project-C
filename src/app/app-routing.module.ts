651c8b138382be6287acb9b49cc81d172e09927a
Marvin-vD <marvin.vd4@gmail.com>
2023-07-04 09:22:26 +0200

    add the links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",component:HomeComponent
+},{
+  path:"about", component:AboutMeComponent
+},{
+  path:"portfolio", component:PortfolioComponent
+},{
+  path:"contact", component:ContactMeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
